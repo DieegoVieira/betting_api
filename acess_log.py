34687d5d8a0b22ed9ae1ef62f34f638abd6974e4
João Pedro S R Lima <joaopedrosrlima@hotmail.com>
2026-05-14 11:36:06 -0300

    fix: garantindo tratamento de erro de escrita e formatacao

--- acess_log.py
+++ acess_log.py
@@ -1,11 +1,8 @@
 def registrar_tentativa(username, ip, status, motivo):
-    # O print manda o log para o painel da Vercel para você monitorar
     print(f"[LOG] Tentativa: {username} | IP: {ip} | Status: {status} | Motivo: {motivo}")
-    
     try:
-        # Funciona no seu computador local, mas vai falhar na Vercel
         with open("tentativas_acesso.log", "a", encoding="utf-8") as arquivo:
             arquivo.write(f"{username} | {ip} | {status} | {motivo}\n")
     except OSError:
-        # Se der erro de Read-only na Vercel, o Python ignora e a API continua rodando lindamente!
+        # Força a Vercel a ignorar o erro de escrita de arquivo e continuar a rota
         pass
